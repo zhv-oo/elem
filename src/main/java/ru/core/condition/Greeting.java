d84e888fe584cd7f104551d07a80848eb7cfa5de
zhv-oo <zhitenev.oleg@yandex.ru>
2024-07-10 10:51:51 +0700

    String.

---
+++ src/main/java/ru/core/condition/Greeting.java
@@ -0,0 +1,14 @@
+package ru.core.condition;
+/**
+ * String.
+ */
+public class Greeting {
+    public static void main(String[] args) {
+        String idea = "I like Java";
+        System.out.println(idea);
+        idea = idea + ", but i'm newbie ";
+        int year = 2024;
+        idea = idea + year;
+        System.out.println(idea);
+    }
+}
