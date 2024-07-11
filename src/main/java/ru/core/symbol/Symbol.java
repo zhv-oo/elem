a57d5a0792e4c0e53b60772594c2aecbc5d5da8d
zhv-oo <zhitenev.oleg@yandex.ru>
2024-07-11 23:12:58 +0700

    Char

---
+++ src/main/java/ru/core/symbol/Symbol.java
@@ -0,0 +1,14 @@
+package ru.core.symbol;
+
+/**
+ * Char.
+ */
+public class Symbol {
+    public static void main(String[] args) {
+        int a = 'A';
+        int b = 'B';
+        System.out.println("A : " + a);
+        System.out.println("B : " + b);
+        System.out.println("A + B : " + (a + b));
+    }
+}
