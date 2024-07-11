0ca58dbaacea965a622ed3d82dd533f7c41dffef
zhv-oo <zhitenev.oleg@yandex.ru>
2024-07-11 23:04:52 +0700

    Increment

---
+++ src/main/java/ru/core/condition/StringRunner.java
@@ -0,0 +1,15 @@
+package ru.core.condition;
+
+/**
+ * Increment
+ */
+public class StringRunner {
+    public static void main(String[] args) {
+        String idea = "I like Java!";
+        System.out.println(idea);
+        idea += " But I am a newbie. ";
+        int year = 2024;
+        idea += year;
+        System.out.println(idea);
+    }
+}
